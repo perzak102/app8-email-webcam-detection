8527121e307587ed2ec2f958150a95bb65226474
perzpio0 <piotr.perz@orange.com>
2024-06-18 23:20:53 +0200

    Day 37: Save image with the object

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
 import cv2
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 fist_frame = None
 status_list = []
+count = 1
 while True:
     status = 0
     check, frame = video.read()
@@ -32,6 +34,11 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}image.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
